45c41af7fba937952014590796a34cbedaf01ace
hishamss <hishamss90@gmail.com>
2021-02-11 14:56:09 -0700

    added modal to save new image

--- src/component/Img.js
+++ src/component/Img.js
@@ -1,11 +1,14 @@
-import React from 'react';
-import {Col, Image} from "react-bootstrap";
-
+import React from "react";
+import { Col, Image } from "react-bootstrap";
+const imgClass = {
+  width: "450px",
+  height: "400px",
+  marginBottom: "5px",
+};
 export const Img = (props) => {
-
-    return(
-        <Col xs={6} md={4}>
-            <Image thumbnail src={props.imageName}/>
-        </Col>
-    )
-}
+  return (
+    <Col xs={6} md={4}>
+      <Image style={imgClass} thumbnail src={props.imageName} />
+    </Col>
+  );
+};
