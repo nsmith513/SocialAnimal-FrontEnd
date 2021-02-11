e1f9cd2d741dc485c405e0a849b8090c924cc526
Kevin Bagnall <bubbagump2020@gmail.com>
2021-02-11 11:30:22 -0600

    file upload button

--- src/component/PetNav.js
+++ src/component/PetNav.js
@@ -1,11 +1,25 @@
 import React from 'react';
-import {Navbar} from "react-bootstrap";
+import {Button, Form, FormControl, Nav, Navbar} from "react-bootstrap";
 
 export const PetNav = () => {
+
+    const onClick = () => {
+
+    }
+
+
     return(
 
         <Navbar bg="light" expand="lg">
             <Navbar.Brand>Petsville</Navbar.Brand>
+            {/*<input type="file" class="form-control" id="imageFile"/>*/}
+            <Form.File id="imageFile"/>
+
+
+
+
+                {/*<input type="file"  name="pic" accept="image/*"/>*/}
+
         </Navbar>
     )
 }
